466b6b299d9d633af029e77e6af2b60f3c846765
Viktor Farcic <viktor@farcic.com>
2024-10-26 01:14:30 +0200

    Progressive delivery

---
+++ manuscript/progressive-delivery/kubecon-slc.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+#########
+# Setup #
+#########
+
+yq --inplace ".istio.enabled = true" \
+    ytt/values-prod.yaml
+
+yq --inplace ".istio.host = \"cncf-demo.$ISTIO_HOST\"" \
+    ytt/values-prod.yaml
